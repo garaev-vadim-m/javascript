4a8bdbdb0634a9218ba5dbd489e97a2c3663dafe
vadim.garaev <68156261+3lyka@users.noreply.github.com>
2023-01-04 17:00:26 +0300

    Переключение по поиску

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ window.onload = () => {
      * @description поиск элементов на странице
      */
     searchPanel.oninput = function () {
+
         const value = this.value.trim();
         const cards = document.querySelectorAll('.cards'); // выбираем все элементы
 
@@ -12,13 +13,66 @@ window.onload = () => {
             cards.forEach(element => {
 
                 if (element.innerText.search(value) !== -1) {
-                    //класс к элементам которые нашли
-                    element.classList.add('search-result');
+                    //класс к элементам которые нашли от 3 символов
+                    if (value.length > 2) {
+                        element.classList.add('search-result');
+                        document.getElementById('foundSearching').classList.remove('display-none');
+                    } else {
+                        element.classList.remove('search-result');
+                        document.getElementById('foundSearching').classList.add('display-none');
+                    }
                     //считаем количество элементов которые нашли
-                    let foundCounter = 0;
                     let allElementsResult = Array.prototype.slice.call(document.querySelectorAll('.search-result'));
                     document.getElementById('allSearchResult').innerHTML = allElementsResult.length.toString();
-                    allElementsResult[foundCounter].classList.add('border');
+
+                    let index = 0;
+                    const nextElement = () => {
+                        const list = document.querySelectorAll('.search-result');
+                        if (index !== 0) {
+                            list[index - 1].classList.remove('active');
+                        }
+                        if (index >= list.length) index = 0;
+                        list[index].classList.add('active');
+                        index += 1;
+
+                        document.getElementById('switchResult').innerHTML = index;
+                    };
+
+                    document.getElementById('select-next').addEventListener('click', () => {
+                        nextElement()
+                    });
+                    //Первая вариация
+                    // let buttonsGroup = {
+                    //     prev: document.getElementById('select-prev'),
+                    //     next: document.getElementById('select-next'),
+                    // };
+                    // //кнопки переходов к элементам
+                    // let items = document.querySelectorAll("#items > div.cards.search-result");
+                    // let index = 0;
+                    //
+                    // document.getElementById('switchResult').innerHTML = index + 1;
+                    //
+                    // buttonsGroup.next.addEventListener('click', function () {
+                    //     index = (index + items.length + 1) % items.length;
+                    //     document.getElementById('switchResult').innerHTML = index + 1;
+                    //     updateSelection();
+                    // });
+                    //
+                    // buttonsGroup.prev.addEventListener('click', function () {
+                    //     index = (index + items.length - 1) % items.length;
+                    //     document.getElementById('switchResult').innerHTML = index + 1;
+                    //     updateSelection();
+                    // });
+                    //
+                    // function updateSelection() {
+                    //
+                    //     let active = document.querySelector('#items > div.cards.search-result.active');
+                    //
+                    //     if (active) active.classList.remove('active');
+                    //
+                    //     items[index].classList.add('active');
+                    // }
+
 
                 } else {
                     /**
@@ -28,12 +82,15 @@ window.onload = () => {
                 }
             });
         } else {
+            //классы которые нужно убирать
             cards.forEach(element => {
                 element.classList.remove('search-result-remove');
                 element.classList.remove('search-result');
                 element.classList.remove('border');
+                element.classList.remove('active');
                 document.getElementById('allSearchResult').innerHTML = '0';
                 document.getElementById('switchResult').innerHTML = '0';
+                document.getElementById('foundSearching').classList.add('display-none');
             });
         }
 
